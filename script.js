46d9d74245af4bb9948f31e6d5beda3b5b6b0287
pikapikamart <80995899+pikapikamart@users.noreply.github.com>
2021-04-20 14:40:06 +0800

    Add files via upload
    Changed the hamburger menu to use button. Added some focus state on the different links in the layout.

--- script.js
+++ script.js
@@ -14,10 +14,5 @@ const overlay = element(".mobile-overlay");
 hamburger.addEventListener("click", ()=>{
     navbar.classList.toggle("show-nav");
     overlay.classList.toggle("show-overlay");
-    
-    if (navbar.classList.contains("show-nav")) {
-        hamburger.setAttribute("src", "images/icon-close.svg");
-    } else {
-        hamburger.setAttribute("src", "images/icon-hamburger.svg");
-    } 
+    hamburger.classList.toggle("active");
 });
